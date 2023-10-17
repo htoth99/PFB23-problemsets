ae92f66ea45d0562a31ea3c2f12cbdd68e938f06
pfb11 <pfb11@info11.cshl.edu>
2023-10-17 11:13:55 -0400

    finished making my first python script

--- python1/myfirstpython.py
+++ python1/myfirstpython.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
-print('my name is hannah')
+import sys
+
+name = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+print('My name is',name,'and my favorite color is',color,'I like to go to the',activity,'and my favorite animal is a',animal)
+
+#print(f'My name is {name}')
+#print('my favorite color is purple')
+#activity = 'my favorite activity is going to the greenhouse'
+#print(activity)
+#print('my favorite animal is a zebra')
