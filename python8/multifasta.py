cfd35f292b2bef5cf7b3ee1b4905ae4ee5226b29
pfb11 <pfb11@info11.cshl.edu>
2023-10-22 16:37:04 -0400

    finished problem 1 of set 8.... finally

--- python8/multifasta.py
+++ python8/multifasta.py
@@ -2,15 +2,42 @@
 import re
 #Problem 1
 
+seq_ID = ''
+nt = ''
 multi_fasta = {}
+count_dict = {}
+
+
 with open('Python_08.fasta','r') as file_obj:
   for line in file_obj:
     line = line.rstrip()
-    find_seq = re.search(r'(>)(\w+\s*?)',line)
-    if find_seq:
-      seqID = find_seq.group(2)
-      print(seqID)
+    find_ID = re.search(r'(>)(\w+\s*?)',line)
+    if find_ID:
+      seqID = find_ID.group(2)
+      multi_fasta[seqID] = ''    
+
+    else:
+      nt = line
+      multi_fasta[seqID] += nt
+#  print(multi_fasta) 
+  for seqID in multi_fasta:
+    seqS = multi_fasta[seqID]
+    if seqID not in count_dict:
+      count_dict[seqID] = {'A' : 0, 'T' : 0, 'G' : 0, 'C' : 0}
+    count_dict[seqID]['A'] = seqS.count('A')
+    count_dict[seqID]['T'] = seqS.count('T')
+    count_dict[seqID]['G'] = seqS.count('G')
+    count_dict[seqID]['C'] = seqS.count('C')
+  print(count_dict)
+  #  Acount = nt.count('A')
+  #  Tcount = nt.count('T')
+  #  Gcount = nt.count('G')
+  #  Ccount = nt.count('C')
+
+      
+#      multi_fasta[seq_ID] = {nt,nt_count}
 
 
 
+#multifasta[seqID][nt]=count
 
